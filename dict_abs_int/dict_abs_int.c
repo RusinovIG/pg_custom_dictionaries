05b702192a174e70219b5b7fa74ea455821c0409
rusinov <rusinov@vl.ru>
2015-07-06 09:57:40 +0600

    inc pointer with pg_mblen

--- dict_abs_int/dict_abs_int.c
+++ dict_abs_int/dict_abs_int.c
@@ -20,17 +20,26 @@ dabs_int_init(PG_FUNCTION_ARGS)
 Datum
 dabs_int_lexize(PG_FUNCTION_ARGS)
 {
-	char	   *in = (char *) PG_GETARG_POINTER(1);
-	char	   *txt = pnstrdup(in, PG_GETARG_INT32(2));
-	TSLexeme   *res = palloc0(sizeof(TSLexeme) * 2);
+	char *in = (char *) PG_GETARG_POINTER(1);
+	char *out = pnstrdup(in, PG_GETARG_INT32(2));;
+	char *start;
+	char *end;
+	TSLexeme *res = palloc0(sizeof(TSLexeme) * 2);
 
 	res[1].lexeme = NULL;
-	if ('-' == txt[0])
-	{
-		// crop "-" symbol
-		*txt = pnstrdup(in + 1, PG_GETARG_INT32(2) - 1);
-	}
-  res[0].lexeme = txt;
+
+	while (*out && t_iseq(out, '-'))
+  		out += pg_mblen(out);
+
+	start = out;
+
+	while (*out)
+    		out += pg_mblen(out);
+
+	end = out;
+	*out = pnstrdup(start, end - start);
+
+  res[0].lexeme = out;
 
 	PG_RETURN_POINTER(res);
 }
